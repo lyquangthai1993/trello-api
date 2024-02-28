f2e8c86b81170ee4e99efcb0a81bf95e5ef0ac86
Ly Quang Thai <thai.ly@masterisehomes.com>
2024-02-28 15:16:30 +0700

    update

---
+++ src/routes/v1/boardRoutes.js
@@ -0,0 +1,23 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+const Router = express.Router()
+
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({
+      messge: 'API GET LIST BOARDS'
+    })
+  })
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({
+      messge: 'API CREATED BOARD'
+    })
+  })
+
+
+export const boardRoutes=Router
