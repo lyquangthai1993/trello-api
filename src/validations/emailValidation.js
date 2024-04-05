8b2c08f2b6698ac914de1efe71168ee361fe3824
lyquangthai <lyquangthai1993@gmail.com>
2024-04-05 21:51:17 +0700

    register and login API

--- src/validations/emailValidation.js
+++ src/validations/emailValidation.js
@@ -1,8 +1,6 @@
-
 import { StatusCodes } from 'http-status-codes';
 import Joi from 'joi';
 import ApiError from '~/utils/ApiError';
-import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators';
 
 const sendEmail = async (req, res, next) => {
   const correctCondition = Joi.object({
